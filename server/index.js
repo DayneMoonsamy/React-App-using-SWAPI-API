24bed584122e655abb5744fe1bd7d3fca13627b7
Dayne <daynemoonsamy1111@gmail.com>
2021-05-31 03:01:12 +0200

    push to main

--- server/index.js
+++ server/index.js
@@ -128,9 +128,7 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers ,introspection: true,
-  playground: true,
-});
+const server = new ApolloServer({ typeDefs, resolvers ,introspection: true, playground: true});
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
